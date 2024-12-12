29fcf3029b8929126fb69e096c01d713920a2b2e
Rogerskelamen <rogerskelamen@gmail.com>
2024-12-12 11:41:40 +0800

    format ErrorOut

--- src/main/scala/app/halftone/errdiff/ErrorOut.scala
+++ src/main/scala/app/halftone/errdiff/ErrorOut.scala
@@ -8,29 +8,29 @@ import tools.bus.BramNativePortFull
 class ErrorOut(config: ErrDiffConfig) extends Module {
   val io = IO(new Bundle {
     val in  = Flipped(Decoupled(new ThreshCalc2ErrorOut(config.posWidth, config.errorWidth)))
-    val pa = Flipped(new BramNativePortFull)
+    val pa  = Flipped(new BramNativePortFull)
     val out = Decoupled(new ErrorOut2WriteBinary(config.posWidth))
   })
 
   // Registers(for value storage and state presentation)
-  val pos    = Reg(UInt(config.posWidth.W))
-  val bval   = Reg(Bool())
-  val errOut = Reg(Vec(4, SInt(config.errorWidth.W)))
-  val busy = RegInit(false.B)
+  val pos         = Reg(UInt(config.posWidth.W))
+  val bval        = Reg(Bool())
+  val errOut      = Reg(Vec(4, SInt(config.errorWidth.W)))
+  val busy        = RegInit(false.B)
   val resultValid = RegInit(false.B)
 
   io.out.valid := resultValid
   io.in.ready  := !busy
   // useless signals
-  io.out.bits  := DontCare
-  io.pa := DontCare
-  io.pa.en := false.B
-  io.pa.we := false.B
+  io.out.bits := DontCare
+  io.pa       := DontCare
+  io.pa.en    := false.B
+  io.pa.we    := false.B
 
-  val diffRight = pos + 1.U
+  val diffRight      = pos + 1.U
   val diffBelowRight = pos + config.imageCol.U + 1.U
-  val diffBelow = pos + config.imageCol.U
-  val diffBelowLeft = pos + config.imageCol.U - 1.U
+  val diffBelow      = pos + config.imageCol.U
+  val diffBelowLeft  = pos + config.imageCol.U - 1.U
   val (cnt, cntWrap) = Counter(busy && !resultValid, 4)
 
   // Emit outputs
@@ -63,10 +63,10 @@ class ErrorOut(config: ErrDiffConfig) extends Module {
   }.otherwise {
     when(io.in.valid) {
       val inBundle = io.in.deq()
-      pos := inBundle.pos
-      bval := inBundle.bval
+      pos    := inBundle.pos
+      bval   := inBundle.bval
       errOut := inBundle.errOut
-      busy := true.B
+      busy   := true.B
     }
   }
 }
