1631f426cc4082fdac9cce4d348743fe1adc75ed
Rogerskelamen <rogerskelamen@gmail.com>
2025-01-07 19:09:06 -0800

    adjust tb_ErrDiffCore

--- src/main/cc/sim/tb_ErrDiffCore/tb_ErrDiffCore.cpp
+++ src/main/cc/sim/tb_ErrDiffCore/tb_ErrDiffCore.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <time.h>
 #include <assert.h>
 #include <math.h>
 
@@ -36,6 +37,11 @@ random_gen(uint32_t max) {
   return rand() % max;
 }
 
+/*
+ * the default `round` function in <math.h> uses
+ * "round half away from zero",
+ * but here needs "round half up"(substitution)
+ */
 static double
 round_half_up(double x) {
   return floor(x+0.5);
@@ -61,12 +67,12 @@ void init_sim() {
 
 void step_and_dump_wave(int n = 1) {
   for (int i = 0; i < n; i++) {
-    top->clk ^= 1;
+    top->clock ^= 1;
     top->eval();
     contextp->timeInc(1);
     m_trace->dump(contextp->time());
 
-    top->clk ^= 1;
+    top->clock ^= 1;
     top->eval();
     contextp->timeInc(1);
     m_trace->dump(contextp->time());
@@ -81,21 +87,21 @@ void exit_sim() {
 int main() {
   init_sim();
 
-  unsigned int seed = 42;
+  // generate random seed
+  unsigned int seed = time(0);
   srand(seed);
-
   // generate gray-image using random generator
   for (int pos = 0; pos < IMG_H*IMG_W; pos++)
     img[pos/IMG_H][pos%IMG_W] = random_gen(0x100);
 
   // init reset
-  top->rstn = 0; top->extn_ready = 1;
+  top->reset = 1; top->in_valid = 0;
   step_and_dump_wave(5);
-  top->rstn = 1; step_and_dump_wave();
+  top->reset = 0; step_and_dump_wave();
 
   // trigger hardware
-  top->extn_ready = 0; step_and_dump_wave();
-  top->extn_ready = 1; step_and_dump_wave();
+  top->in_valid = 1; step_and_dump_wave();
+  top->in_valid = 0;
 
   for (int i = 0; i < IMG_H; i++) {
     for (int j = 0; j < IMG_W; j++) {
@@ -112,13 +118,13 @@ int main() {
       errdiff[2] = (int)round_half_up(err * 5.0/16);
       errdiff[3] = (int)round_half_up(err * 3.0/16);
 
-      // execute simulation
-      top->pb_dout = errCache[i][j]; step_and_dump_wave(2);
-      top->read_resp_valid = 1; top->read_resp_data = pix << ((j%4) * 8);
-      step_and_dump_wave();
-      top->read_resp_valid = 0; top->read_resp_data = 0; step_and_dump_wave(2);
+      // Execute simulation
+      top->cache_pb_dout = errCache[i][j];
+      top->img_pb_dout = pix;
 
-      step_and_dump_wave(); // enter ErrorOut
+      while(!top->rootp->ErrDiffCore__DOT__threshCalc__DOT__resultValid)
+        step_and_dump_wave(); // enter ErrorOut
+      step_and_dump_wave(); // get bval
       uint8_t bval = top->rootp->ErrDiffCore__DOT__errorOut__DOT__bval * 0xff;
       AssertExit(bval == img[i][j], "bval[%#x] != result[%#x], at row[%d] column[%d]", bval, img[i][j], i, j);
 
@@ -127,23 +133,20 @@ int main() {
       errOut[1] = (int8_t)top->rootp->ErrDiffCore__DOT__errorOut__DOT__errOut_1;
       errOut[2] = (int8_t)top->rootp->ErrDiffCore__DOT__errorOut__DOT__errOut_2;
       errOut[3] = (int8_t)top->rootp->ErrDiffCore__DOT__errorOut__DOT__errOut_3;
-      for (int k = 0; k < 4; k++) {
+      for (int k = 0; k < 4; k++)
         AssertExit(errOut[k] == errdiff[k], "errOut%d[%d] != result[%d], at row[%d] column[%d], err = %d", k, errOut[k], errdiff[k], i, j, err);
-      }
 
-      top->pa_dout = check_bound_then_acc(i, j+1); step_and_dump_wave(); // right
-      top->pa_dout = check_bound_then_acc(i+1, j); step_and_dump_wave(); // below
-      top->pa_dout = check_bound_then_acc(i+1, j-1); step_and_dump_wave(); // belowLeft
+      top->cache_pa_dout = check_bound_then_acc(i, j+1); step_and_dump_wave(); // right
+      top->cache_pa_dout = check_bound_then_acc(i+1, j); step_and_dump_wave(); // below
+      top->cache_pa_dout = check_bound_then_acc(i+1, j-1); step_and_dump_wave(); // belowLeft
       check_bound_then_acc(i, j+1, errdiff[0]);
       check_bound_then_acc(i+1, j+1, errdiff[1]);
       check_bound_then_acc(i+1, j, errdiff[2]);
       check_bound_then_acc(i+1, j-1, errdiff[3]);
       step_and_dump_wave(4);
 
-      // WriteBinary
-      step_and_dump_wave();
-      top->write_resp_valid = 1; step_and_dump_wave();
-      top->write_resp_valid = 0; step_and_dump_wave();
+      // WriteBinary to PixelGet
+      step_and_dump_wave(3);
     }
   }
 
