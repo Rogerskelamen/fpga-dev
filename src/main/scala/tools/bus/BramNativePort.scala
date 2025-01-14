54f62b76d5d97bb4745c7ae618dfe22a2ea553d0
Rogerskelamen <rogerskelamen@gmail.com>
2025-01-14 15:16:33 +0800

    change bram dual ports definition

--- src/main/scala/tools/bus/BramNativePort.scala
+++ src/main/scala/tools/bus/BramNativePort.scala
@@ -7,8 +7,9 @@ object BramNativePortParam {
   val addrWidth = 12
 }
 
-// It's in the view of BRAM, So
-// Direction: from Slave to Master
+/** It's in the view of BRAM, So
+  * Direction: from Slave to Master
+  */
 trait HasWriteEnable {
   def dwidth: Int = BramNativePortParam.dataWidth
   val din = Input(UInt(dwidth.W))
@@ -17,12 +18,25 @@ trait HasWriteEnable {
 
 class BramNativePort(val dwidth: Int = BramNativePortParam.dataWidth,
                      val awidth: Int = BramNativePortParam.addrWidth)
-  extends Bundle {
+    extends Bundle {
   val en   = Input(Bool())
   val dout = Output(UInt(dwidth.W))
   val addr = Input(UInt(awidth.W))
 }
 
-class BramNativePortFull(override val dwidth: Int = BramNativePortParam.dataWidth,
-                         override val awidth: Int = BramNativePortParam.addrWidth)
-  extends BramNativePort with HasWriteEnable
+class BramNativePortFull(
+  override val dwidth: Int = BramNativePortParam.dataWidth,
+  override val awidth: Int = BramNativePortParam.addrWidth)
+    extends BramNativePort
+    with HasWriteEnable
+
+/** Bram Port A: serve for writing
+  * Bram Port B: Serve for reading
+  */
+class BramNativeDualPorts(
+  val dwidth: Int = BramNativePortParam.dataWidth,
+  val awidth: Int = BramNativePortParam.addrWidth)
+    extends Bundle {
+  val pb = Flipped(new BramNativePortFull(dwidth, awidth))
+  val pa = Flipped(new BramNativePortFull(dwidth, awidth))
+}
