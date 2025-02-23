6cd14560c1e9ba1c7f4be2b4d3a530ce53620fb4
Rogerskelamen <rogerskelamen@gmail.com>
2025-02-23 16:14:30 +0800

    change name for signal in RandomEmitter

--- src/main/scala/tools/RandomEmitter.scala
+++ src/main/scala/tools/RandomEmitter.scala
@@ -15,9 +15,9 @@ class RandomEmitter(val dwidth: Int) extends Module {
     val out = Output(Bool())
   })
 
-  val rfsr = RegInit(3.U(dwidth.W))
-  val xor = rfsr(0) ^ rfsr(2) ^ rfsr(3) ^ rfsr(5)
-  rfsr := Cat(xor, rfsr(dwidth-1, 1))
+  val lfsr = RegInit(3.U(dwidth.W))
+  val xor = lfsr(0) ^ lfsr(2) ^ lfsr(3) ^ lfsr(5)
+  lfsr := Cat(xor, lfsr(dwidth-1, 1))
 
-  io.out := rfsr(0)
+  io.out := lfsr(0)
 }
